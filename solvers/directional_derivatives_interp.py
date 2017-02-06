5d54701aa37e4673f9f75f4ed6fcd2c15fd9cf44
Chris Finlay <christopher.finlay@gmail.com>
2017-02-06 14:12:52 -0500

    Functions for first order derivatives.

--- solvers/directional_derivatives_interp.py
+++ solvers/directional_derivatives_interp.py
@@ -1,5 +1,152 @@
 import numpy as np
 
+# Stencil vectors
+__stencil = np.array([[  1,  0],
+                      [  1,  1],
+                      [  0,  1],
+                      [ -1,  1],
+                      [ -1,  0],
+                      [ -1, -1],
+                      [ -1,  0],
+                      [  1, -1]])
+
+# Arrangement of neighbouring stencil vectors: basis vector first
+__nb_pts = np.array([[[1, 0], [1,1]],
+                     [[0, 1], [1,1]],
+                     [[0, 1], [-1,1]],
+                     [[-1,0], [-1,1]],
+                     [[-1,0], [-1,-1]],
+                     [[0,-1], [-1,-1]],
+                     [[0,-1], [1,-1]],
+                     [[1,0], [1,-1]]])
+
+def d1da(U,dx,direction="both"):
+    """
+    d1da(U,dx,d,direction="both") 
+    
+    Calculate descent and ascent directions respectively minimizing and
+    maximizing 
+        <grad u, p>, st ||p|| = 1.
+    
+    Parameters
+    ----------
+    u : array_like
+        Function values at grid points.
+    dx : scalar
+        Uniform grid resolution.
+    string : string
+        Specify which direction to retrieve: "descent", "ascent", or "both".
+
+    Returns
+    -------
+    Value : a list, or an array
+        If direction="both", a list containing the value <grad u, p>. The first
+        entry of the list gives the value when p is a descent direction; the second for p
+        an ascent direction.
+        If direction!="both", then an array of the specified maximal or minimal value.
+    Theta : a list, or an array
+        If direction="both", a list containing the angle of the descent and ascent directions,
+        with the angle of the descent direction first.
+        If direction!="both", then an array of the angle of the corresponding direction.
+    """
+    Nx, Ny = U.shape
+    Dv = np.zeros((16,Nx-2,Ny-2)) 
+    theta = np.zeros((16,Nx-2,Nx-2))
+
+    # Index excluding the boundary
+    I = np.arange(1,Nx-1,1,dtype=np.intp) 
+    J = np.arange(1,Ny-1,1,dtype=np.intp)
+    
+    A = U[1:-1,1:-1]
+    for k, (p,nbs) in enumerate(zip(__stencil,__nb_pts)):
+
+        # Exact directional derivative (and angle) in the stencil
+        Dv[k,:,:] = (-A + U[np.ix_(I+p[0],J+p[1])])/(dx*np.sqrt(p[0]**2 + p[1]**2))
+        theta[k,:,:] = np.arctan2(p[1],p[0])
+
+        # Now check directional derivatives via linear interpolation.
+        # First get neighbouring direction vectors in the stencil
+        v = nbs[0,:] # +/- basis vector
+        w = nbs[1,:] # diagonal
+
+        # Function values in the stencil
+        B = U[np.ix_(I+v[0],J+v[1])]
+        C = U[np.ix_(I+w[0],J+w[1])]
+
+        # Calculate angle of possible minimizer
+        th = np.arctan2(B-C, A-B)
+        th = np.mod(th,np.pi)
+        th[th>np.pi/4] = 0
+        t = np.tan(th) # convex parameter
+
+        # Candidate for minimizer
+        Dv[k+8,:,:] = (-A + t*C + (1-t)*B)/(dx*np.sqrt(1+t**2))
+
+        # Angle of candidate minimizer
+        if k==0:
+            theta[k+8,:,:] = np.arctan(t)
+        elif k==1:
+            theta[k+8,:,:] = np.pi/2-np.arctan(t)
+        elif k==2:
+            theta[k+8,:,:] = np.pi/2+np.arctan(t)
+        elif k==3:
+            theta[k+8,:,:] = np.pi-np.arctan(t)
+        elif k==4:
+            theta[k+8,:,:] = np.pi+np.arctan(t)
+        elif k==5:
+            theta[k+8,:,:] = np.pi*3/2-np.arctan(t)
+        elif k==6:
+            theta[k+8,:,:] = np.pi*3/2+np.arctan(t)
+        elif k==7:
+            theta[k+8,:,:] = -np.arctan(t)
+    theta = np.mod(theta,np.pi*2)
+
+    [i,j] = np.indices((Nx-2,Ny-2))
+    if direction=="both":
+        kmin = Dv.argmin(0)
+        dmin = Dv[kmin,i,j]
+        theta_min = theta[kmin,i,j]
+
+        kmax = Dv.argmax(0)
+        dmax = Dv[kmax,i,j]
+        theta_max = theta[kmax,i,j]
+
+        Value = dmin, dmax
+        Theta = theta_min, theta_max
+
+        return Value, Theta
+    elif direction=="descent":
+        kmin = Dv.argmin(0)
+        dmin = Dv[kmin,i,j]
+        theta_min = theta[kmin,i,j]
+
+        return dmin, theta_min
+    elif direction=="ascent":
+        kmax = Dv.argmax(0)
+        dmax = Dv[kmax,i,j]
+        theta_max = theta[kmax,i,j]
+
+        return dmax, theta_max
+
+def d1descent(U,dx):
+    """
+    d2descent(u,dx) 
+    
+    Compute the minimal value of <grad u, p> st ||p||=1.
+    Equivalent to calling d2da(u,dx,direction="descent")
+    """
+    Value, Theta = d1da(U,dx,direction="descent")
+    return Value, Theta
+
+def d1ascent(U,dx):
+    """
+    d2ascent(u,dx) 
+    
+    Compute the maximal value of <grad u, p> st ||p||=1.
+    Equivalent to calling d2da(u,dx,direction="ascent")
+    """
+    Value, Theta = d1da(U,dx,direction="ascent")
+    return Value, Theta
 
 def d1(U,theta,dx):
     """
@@ -25,16 +172,6 @@ def d1(U,theta,dx):
     Nx, Ny = U.shape
 
     theta = np.mod(theta, 2*np.pi)
-
-    # Arrangement of neighbouring stencil vectors: basis vector first
-    nb_pts = np.array([[[1, 0], [1,1]],
-                      [[0, 1], [1,1]],
-                      [[0, 1], [-1,1]],
-                      [[-1,0], [-1,1]],
-                      [[-1,0], [-1,-1]],
-                      [[0,-1], [-1,-1]],
-                      [[0,-1], [1,-1]],
-                      [[1,0], [1,-1]]])
 
     # Parameter for adding convex combination of neighbouring stencil vectors
     th = np.mod(theta, np.pi)
@@ -68,7 +205,7 @@ def d1(U,theta,dx):
              np.logical_and(theta >=7*np.pi/4, theta < 2*np.pi))
 
     A = U[1:-1,1:-1]
-    for ix, nbs in zip(Cases, nb_pts):
+    for ix, nbs in zip(Cases, __nb_pts):
         if (ix.size != 1) or ix:
             # Neighbouring direction vectors in the stencil
             v = nbs[0,:] # basis vector
@@ -84,20 +221,6 @@ def d1(U,theta,dx):
                 DU[ix] = (-2*A[ix] + (1-t[ix])*B[ix] +t[ix]*C[ix])/(np.sqrt((1+t[ix]**2))*dx)
 
     return DU
-    
-
-
-# Stencil vectors -- only half the stencil needed for second derivative
-__stencil = np.array([[1, 0],
-                    [1, 1],
-                    [0, 1],
-                    [-1,1]])
-
-# Arrangement of neighbouring stencil vectors
-__nb_pts= np.array([[[1, 0], [1,1]],
-                    [[0, 1], [1,1]],
-                    [[0, 1], [-1,1]],
-                    [[-1,0], [-1,1]]])
 
 def d2(U,theta,dx):
     """
@@ -151,7 +274,7 @@ def d2(U,theta,dx):
              np.logical_and(theta >=3*np.pi/4, theta < np.pi))
 
     A = U[1:-1,1:-1]
-    for ix, nbs in zip(Cases, __nb_pts):
+    for ix, nbs in zip(Cases, __nb_pts[0:4,:,:]):
         if (ix.size != 1) or ix:
             # Neighbouring direction vectors in the stencil
             v = nbs[0,:] # basis vector
@@ -204,12 +327,12 @@ def d2eigs(U,dx,eigs="both"):
 
     A = U[1:-1,1:-1]
 
-    for k, (p,nbs) in enumerate(zip(__stencil,__nb_pts)):
+    for k, (p,nbs) in enumerate(zip(__stencil[0:4,:],__nb_pts[0:4,:])):
 
         # Exact directional derivative (and angle) in the stencil
         Dvv[k,:,:] = ((-2*A + U[np.ix_(I+p[0],J+p[1])] + U[np.ix_(I-p[0],J-p[1])]) 
                         / ((p[0]**2 + p[1]**2)*dx**2))
-        theta[k,:,:] = np.arctan2(p[0],p[1])
+        theta[k,:,:] = np.arctan2(p[1],p[0])
 
         # Now check directional derivatives via linear interpolation.
         # First get neighbouring direction vectors in the stencil
@@ -217,17 +340,17 @@ def d2eigs(U,dx,eigs="both"):
         w = nbs[1,:] # either (1,1) or (-1,1)
 
         # Sum of antipodal function values in the stencil
-        C = U[np.ix_(I+v[0],J+v[1])] + U[np.ix_(I-v[0],J-v[1])]
-        B = U[np.ix_(I+w[0],J+w[1])] + U[np.ix_(I-w[0],J-w[1])]
+        B = U[np.ix_(I+v[0],J+v[1])] + U[np.ix_(I-v[0],J-v[1])]
+        C = U[np.ix_(I+w[0],J+w[1])] + U[np.ix_(I-w[0],J-w[1])]
 
         # Calculate angle of possible minimizer
-        th = 1/2 * np.arctan2(C-B, 2*A-C)
+        th = 1/2 * np.arctan2(B-C, 2*A-B)
         th = np.mod(th,np.pi/2)
         th[th>np.pi/4] = 0
         t = np.tan(th) # convex parameter
 
         # Candidate for minimizer
-        Dvv[k+4,:,:] = (-2*A + t*B + (1-t)*C)/((1+t**2)* dx**2)
+        Dvv[k+4,:,:] = (-2*A + t*C + (1-t)*B)/((1+t**2)* dx**2)
 
         # Angle of candidate minimizer
         if k==0:
--- tests/test_directional_derivatives_interp.py
+++ tests/test_directional_derivatives_interp.py
@@ -9,6 +9,8 @@ Nx = 21                      #grid size
 dx = 2./(Nx-1)               #grid resolution
 x = np.linspace(-1.,1.,num = Nx) #gridpoints in x coordinate
 X, Y = np.meshgrid(x,x,sparse=False,indexing='ij') # create x & y gridpoints, with matrix indexing
+Xint = X[1:-1,1:-1]
+Yint = Y[1:-1,1:-1]
 
 
 U = X**2 - Y**2
@@ -37,8 +39,17 @@ class TestOnGrid:
     def test_d1(self):
         Ux = ddi.d1(U2,0,dx)
         Uy = ddi.d1(U2,np.pi/2,dx)
+        U_1_1 = ddi.d1(U2,np.pi*5/4,dx)
+        U1_1 = ddi.d1(U2,np.pi*7/4,dx)
         assert ((np.abs(Ux-1) <1e-13).all() and 
-                (np.abs(Uy+1) <1e-13).all())
+                (np.abs(Uy+1) <1e-13).all() and
+                (np.abs(U1_1-np.sqrt(2)) <1e-13).all())
+    def test_d1da(self):
+        V, T = ddi.d1da(U2,dx)
+        assert ((np.abs(V[0]+np.sqrt(2))<1e-13).all() and
+                (np.abs(V[1]-np.sqrt(2)<1e-13).all()))
+        assert ((np.abs(T[0] - np.pi*3/4)<1e-13).all() and
+                (np.abs(T[1] - np.pi*7/4)<1e-13).all())
 
 class TestOffGrid:
     def test_d2eigs(self):
