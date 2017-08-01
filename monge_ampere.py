08d46fa54e11aabf84db3dfe7bd8664556e5ee4f
Chris Finlay <christopher.finlay@gmail.com>
2017-08-01 11:37:14 -0400

    Added Neumann BC for Monge Ampere

--- monge_ampere.py
+++ monge_ampere.py
@@ -46,7 +46,7 @@ def operator(Grid,W,jacobian=True,fdmethod='interpolate'):
     FW = lmin*lmax
 
     #dx = Grid._VDist.min()
-    dx = np.max([Grid.min_radius,Grid.min_edge_length])
+    dx = np.max([Grid.min_radius,Grid.min_interior_nb_dist])
     dt = 1/2 * dx**2 * 1/(lmin+lmax).max()
 
     if jacobian:
@@ -56,13 +56,21 @@ def operator(Grid,W,jacobian=True,fdmethod='interpolate'):
 
     return FW, Jac, dt
 
-def solve(Grid,f,g,U0=None,fdmethod='interpolate', solver='newton',**kwargs):
+def solve(Grid,f,dirichlet=None,neumann=None,
+        U0=None,fdmethod='interpolate', solver='euler',**kwargs):
     r"""
-    Solve the Monge-Ampere equation,
+    Solve the Monge-Ampere equation, either with Dirichlet BC
     \[
         -\det [D^2u] = f, \,x \in \Omega \\
                    u = g, \,x \in \partal \Omega
-                   u convex.
+                   u convex,
+    \]
+    or with Neumann BC
+    \[
+        -\det [D^2u] = f, \,x \in \Omega \\
+        \frac{\partial u}{\partial n} = h,\, x \in \partal \Omega.
+        u convex,
+    where n is the outward normal.
     \]
 
     Parameters
@@ -71,8 +79,10 @@ def solve(Grid,f,g,U0=None,fdmethod='interpolate', solver='newton',**kwargs):
         The mesh of grid points.
     f : array_like or function
         Forcing function on interior.
-    g : array_like or function
+    dirichlet : array_like or function
         Dirichlet boundary condition.
+    neumann : array_like or function
+        Neumann boundary condition.
     U0 : array_like
         Initial guess. Optional.
     fdmethod : string
@@ -96,25 +106,50 @@ def solve(Grid,f,g,U0=None,fdmethod='interpolate', solver='newton',**kwargs):
 
     Notes
     -----
-    The parameters f and g may be either numpy arrays or functions. If functions,
-    they in (N, dim) arrays and return (N,) array.
-    """
+    The parameters f, dirichlet, and neumann may be either numpy arrays, or functions. If
+    functions, they take in a (N, dim) array and return (N,) arrays.
+   """
+    g = dirichlet
+    h = neumann
 
-    if callable(f):
-        f = f(Grid.interior_points)
+    if g is None and h is None:
+        raise ValueError('Please specify a boundary condition')
+    elif g is not None and h is not None:
+        raise ValueError('Cannot have both Dirichet and Neumann boundary conditions')
 
     if callable(g):
         g = g(Grid.bdry_points)
 
+    if callable(f):
+        f = f(Grid.interior_points)
+
+    if callable(h):
+        h = h(Grid.bdry_points)
+
+    if h is not None:
+        if fdmethod=='interpolate':
+            d1n = ddi.d1(Grid, -Grid.bdry_normals, domain='boundary')[1]
+        elif fdmethod=='grid':
+            d1n = ddg.d1(Grid, -Grid.bdry_normals, domain='boundary')[1]
+
     # Initial guess
     if U0 is None:
         U0 = np.zeros(Grid.num_points)
-        U0[Grid.bdry] = g
+        if g is not None:
+            U0[Grid.bdry] = g
+        else:
+            U0[Grid.bdry] = 1.0
 
     # Forcing function over the whole domain
     F = np.zeros(Grid.num_points)
-    F[Grid.bdry] = -g
+    if h is not None:
+        F[Grid.bdry] = h
+    else:
+        F[Grid.bdry] = -g
     F[Grid.interior] = f
+
+    if h is not None:
+        dt_bdry = np.max([Grid.dist_to_bdry,Grid.min_radius])
 
     # Define the operator on the whole domain
     def G(W, jacobian=True):
@@ -122,15 +157,29 @@ def solve(Grid,f,g,U0=None,fdmethod='interpolate', solver='newton',**kwargs):
 
         GW = np.zeros(Grid.num_points)
         GW[Grid.interior] = -MA
-        GW[Grid.bdry] = -W[Grid.bdry]
+
+        if h is not None:
+            GW[Grid.bdry] = -d1n.dot(W)
+        else:
+            GW[Grid.bdry] = -W[Grid.bdry]
 
         if jacobian:
             Jac = sparse.diags(np.full(Grid.num_points,-1.0),format='csr')
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
                 Jac[Grid.interior]=-M
+            if h is not None:
+                with warnings.catch_warnings():
+                    warnings.simplefilter("ignore")
+                    Jac[Grid.bdry] = -d1n
         else:
             Jac=None
+
+
+        if h is not None:
+            dt = np.min([dt, dt_bdry])
+
+
 
         return (GW - F), Jac, dt
 
@@ -140,6 +189,9 @@ def solve(Grid,f,g,U0=None,fdmethod='interpolate', solver='newton',**kwargs):
             op = G(W,jacobian=False)
             return op[0], op[2]
 
-        return solvers.euler(U0, G_, **kwargs)
+        if h is None:
+            return solvers.euler(U0, G_, **kwargs)
+        else:
+            return solvers.euler(U0, G_, zeromean=True,**kwargs)
     elif solver=="newton":
         return solvers.newton(U0, G, scipysolver='lsmr',euler_ratio=1,**kwargs)
