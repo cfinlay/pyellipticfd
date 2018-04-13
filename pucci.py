94419b6e596923a50c31e6bdf48fd1d35d668f82
Chris Finlay <christopher.finlay@gmail.com>
2018-04-13 15:13:51 -0400

    Pucci equation

--- pucci.py
+++ pucci.py
@@ -2,11 +2,11 @@ import warnings
 import numpy as np
 import scipy.sparse as sparse
 
-from pyellipticfd import ddi, ddg, solvers
+from pyellipticfd import ddi, ddg, ddf, solvers
 
-def operator(Grid, U, jacobian=True,fdmethod='interpolate'):
+def operator(Grid, W, A, jacobian=True,fdmethod='interpolate'):
     """
-    Return the finite difference infinity Laplace operator on arbitrary grids.
+    Return the finite difference Pucci operator on arbitrary grids.
 
     Parameters
     ----------
@@ -14,10 +14,13 @@ def operator(Grid, U, jacobian=True,fdmethod='interpolate'):
         The mesh of grid points.
     U : array_like
         The function values.
+    A : list
+        A list of numpy arrays, or scalars. The arrays correspond to the spatial coefficients
+        of the Hessian's eigenvalues. Ordered from maximal to minimal eigenvalues.
     jacobian : boolean
         Whether to calculate the finite difference matrix.
     fdmethod : string
-        Which finite difference method to use. Either 'interpolate' or 'grid'.
+        Which finite difference method to use. Either 'interpolate', 'grid' or 'froese'.
 
     Returns
     -------
@@ -27,43 +30,40 @@ def operator(Grid, U, jacobian=True,fdmethod='interpolate'):
         The finite difference matrix of the operator. Set to None if
         jacobian=False
     """
+    if Grid.dim==3:
+        raise NotImplementedError("Pucci operator not yet implemented in 3d.")
 
     # Construct the finite difference operator, get value of first
     # derivative in minimal and maximal gradient directions
-    if fdmethod=='interpolate':
-        P = ddi.d1grad(Grid,U,control=True,jacobian=jacobian)
-        M = ddi.d1grad(Grid,-U, control=True,jacobian=jacobian)
-    elif fdmethod=='grid':
-        P = ddg.d1grad(Grid,U,control=True,jacobian=jacobian)
-        M = ddg.d1grad(Grid,-U, control=True,jacobian=jacobian)
-    d1p, d1m = P[0], -M[0]
+    if fdmethod=='grid':
+        op = ddg.d2eigs(Grid,W,jacobian=jacobian,control=False)
+    elif fdmethod=='interpolate':
+        op = ddi.d2eigs(Grid,W,jacobian=jacobian,control=False)
+    elif fdmethod=='froese':
+        op = ddf.d2eigs(Grid,W,jacobian=jacobian,control=False)
 
-    vpn = np.linalg.norm(P[2],axis=1)
-    vmn = np.linalg.norm(M[2],axis=1)
+    (lmin, Mmin, _), (lmax, Mmax, _) = op
 
-    scaling = 2/(vpn + vmn)
-    val = scaling*(d1p + d1m)
 
-    if  jacobian:
-        Dm, Dp = M[1], P[1]
-        M = sparse.diags(scaling).dot(Dm+Dp)
+    val = A[0]*lmax + A[1]*lmin
+
+    if jacobian:
+        if np.isscalar(A[0]) and np.isscalar(A[1]):
+            M = A[0]*Mmax + A[1]*Mmin
+        else:
+            M = sparse.diags(A[0]).dot(Mmax) + sparse.diags(A[1]).dot(Mmin)
     else:
         M = None
 
-    return val, M
+    return -val, -M
 
-def solve(Grid,f,dirichlet=None,neumann=None,U0=None,fdmethod='interpolate',
+def solve(Grid,f,dirichlet,A=(2,1),U0=None,fdmethod='interpolate',
           solver="newton",**kwargs):
     r"""
-    Solve either the infinity Laplacian with Dirichlet BC
+    Solve the Pucci equation with Dirichlet BC
     \[
-        -\Delta_\infty u = f, \,x \in \Omega \\
-                       u = g, \,x \in \partal \Omega,
-    \]
-    or with Neumann BC
-    \[
-        -\Delta_infty u = f, \,x \in \Omega \\
-        \frac{\partial u}{\partial n} = h,\, x \in \partal \Omega.
+        A[0] \lambda_1[u] + A[2]\lambda_2[u] = f, \,x \in \Omega \\
+                       u = g, \,x \in \partal \Omega
     \]
 
     Parameters
@@ -74,8 +74,8 @@ def solve(Grid,f,dirichlet=None,neumann=None,U0=None,fdmethod='interpolate',
         The forcing function on the interior.
     dirichlet : array_like or function
         Dirichlet boundary condition.
-    neumann : array_like or function
-        Neumann boundary condition.
+    A : tuple of scalars or array_like
+        Coefficient of maximal eigenvalue
     U0 : array_like
         Initial guess. Optional.
     fdmethod : string
@@ -99,16 +99,10 @@ def solve(Grid,f,dirichlet=None,neumann=None,U0=None,fdmethod='interpolate',
 
     Notes
     -----
-    The parameters f, dirichlet, and neumann may be either numpy arrays, or functions. If
+    The parameters f anddirichlet may be either numpy arrays, or functions. If
     functions, they take in a (N, dim) array and return (N,) arrays.
     """
     g = dirichlet
-    h = neumann
-
-    if g is None and h is None:
-        raise ValueError('Please specify a boundary condition')
-    elif g is not None and h is not None:
-        raise ValueError('Cannot have both Dirichet and Neumann boundary conditions')
 
     if callable(f):
         f = f(Grid.interior_points)
@@ -116,21 +110,10 @@ def solve(Grid,f,dirichlet=None,neumann=None,U0=None,fdmethod='interpolate',
     if callable(g):
         g = g(Grid.bdry_points)
 
-    if callable(h):
-        h = h(Grid.bdry_points)
-
-    if h is not None:
-        if fdmethod=='interpolate':
-            d1n = ddi.d1(Grid, -Grid.bdry_normals, domain='boundary')
-        elif fdmethod=='grid':
-            d1n = ddg.d1(Grid, -Grid.bdry_normals, domain='boundary')
 
     # Forcing function over the whole domain
     F = np.zeros(Grid.num_points)
-    if h is not None:
-        F[Grid.bdry] = h
-    else:
-        F[Grid.bdry] = g
+    F[Grid.bdry] = g
     F[Grid.interior] = f
 
     dx = np.max([Grid.min_radius,Grid.min_interior_nb_dist])
@@ -139,15 +122,12 @@ def solve(Grid,f,dirichlet=None,neumann=None,U0=None,fdmethod='interpolate',
     # Operator over whole domain
     def G(W, jacobian=True):
 
-        inf_Lap, M = operator(Grid, W, jacobian=jacobian,fdmethod=fdmethod)
+        val, M = operator(Grid, W, A, jacobian=jacobian,fdmethod=fdmethod)
 
         GW = np.zeros(Grid.num_points)
-        GW[Grid.interior] = -inf_Lap
+        GW[Grid.interior] = -val
 
-        if h is not None:
-            GW[Grid.bdry] = d1n.dot(W)
-        else:
-            GW[Grid.bdry] = W[Grid.bdry]
+        GW[Grid.bdry] = W[Grid.bdry]
 
         if not jacobian:
             Jac = None
@@ -157,10 +137,6 @@ def solve(Grid,f,dirichlet=None,neumann=None,U0=None,fdmethod='interpolate',
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
                 Jac[Grid.interior]=-M
-            if h is not None:
-                with warnings.catch_warnings():
-                    warnings.simplefilter("ignore")
-                    Jac[Grid.bdry] = d1n
 
         return GW - F, Jac, dt
 
@@ -178,12 +154,7 @@ def solve(Grid,f,dirichlet=None,neumann=None,U0=None,fdmethod='interpolate',
             op = G(W, jacobian=False)
             return op[0], op[2]
 
-        if h is not None:
-            # With Neumann conditions, return the solution with zero mean
-            # (where each point has equal weight)
-            return solvers.euler(U0, G_, zeromean=True,**kwargs)
-        else:
-            return solvers.euler(U0, G_, **kwargs)
+        return solvers.euler(U0, G_, **kwargs)
 
     elif solver=="newton":
         return solvers.NewtonEulerLS(U0, G, **kwargs)
